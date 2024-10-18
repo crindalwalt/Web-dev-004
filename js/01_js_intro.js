3dda5a830e8d2ad0e02ed9b478f3e26ecb828ed7
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2024-10-18 22:15:08 +0500

    js started

---
+++ js/01_js_intro.js
@@ -0,0 +1,19 @@
+console.log("Shahzad Farooq");
+
+// common concepts in any language
+
+/**
+ * Variables
+ * Datatypes
+ * If else
+ * switch
+ * loops
+ * functions
+ * OOP
+ *
+ * ------------
+ * DOM manuplation
+ * Https request
+ *
+ *
+ */
---
+++ js/02_js_variable.js
@@ -0,0 +1,21 @@
+console.log("Variable");
+
+/**
+ * variable name should not start with a number (alphabets or underscore)
+ * start a variable with let or const initialzers
+ *
+ *
+ * @type {number}
+ */
+let age = 12;
+console.log(age)
+age = 13;
+let name = "Shahzad Farooq";
+var nombre = "Hala Madrid";
+let friendName = "Zaid";
+const PI = 3.1415;
+
+const apiKey = "ad20c9595803f9641f6370b142b17096-d010bdaf-23ab9953";
+// PI = 4.3245;
+
+console.log(typeof(age))
