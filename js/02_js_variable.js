ad050c9fd5e5955d1ed044a65b1ee5dc0b5f2217
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2024-10-22 21:49:54 +0500

    operators done

--- js/02_js_variable.js
+++ js/02_js_variable.js
@@ -14,8 +14,8 @@ let name = "Shahzad Farooq";
 var nombre = "Hala Madrid";
 let friendName = "Zaid";
 const PI = 3.1415;
-
+let isMarried = false;
 const apiKey = "ad20c9595803f9641f6370b142b17096-d010bdaf-23ab9953";
-// PI = 4.3245;
+
 
 console.log(typeof(age))
---
+++ js/03_operators.js
@@ -0,0 +1,69 @@
+console.log("Operators in JavaScript");
+
+/**
+ * Operators in JS
+ * 4 types of operators in JS
+ * 1) Arithmatics operator (+,-,*,/,%)
+ * 2) Camparision operator (<,>,<=,>=,==,!=,===)
+ * 3) Assignement operator (=,+=,-=.*=,/=)
+ * 4) Logical operator (&&,||,!)
+ *
+ */
+
+// ? Arithmatics operators
+let num1 = 34;
+let num2 = 20;
+let result = num1 + num2;
+result = num1 - num2;
+result = num1 * num2;
+result = num1 / num2;
+result = num1 % num2;
+// console.log(result);
+
+//? Camparision operator
+
+num1 = 34;
+num2 = 20;
+let age = 23;
+let secondAge = "23";
+result = num1 < num2;
+result = num2 > num2;
+result = num2 >= num2;
+result = num1 <= num2;
+result = num1 == num2;
+result = num1 != num2;
+result = num1 === num2;
+
+let newResult = age === secondAge;
+// console.log(newResult);
+
+
+
+
+let n1 = 50;
+let n2 = 160;
+console.log("n1 = " + n1)
+console.log("n2 = " + n2)
+// n1 = n1 + 70;
+n1 += 70;
+
+
+console.log("n1 = " + n1)
+console.log("n2 = " + n2)
+
+n1 -= 20;
+console.log("n1 = " + n1)
+
+let n3 = 45;
+n1 = n3;
+n1 += 30;
+console.log("n1 = " + n1)
+
+
+
+
+// ? Logical operator 
+
+let condition = 34 > 45 || num1 > num2;
+console.log(!condition)
+
